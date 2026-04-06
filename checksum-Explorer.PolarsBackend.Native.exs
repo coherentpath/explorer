c259126577efb21af3f322939334690d0ae1ec6d
Douglas Manzelmann <dmanzelmann@movableink.com>
2026-04-06 13:39:39 -0400

    add checksum file for precompiled NIFs

---
+++ checksum-Explorer.PolarsBackend.Native.exs
@@ -0,0 +1,5 @@
+%{
+  "https://github.com/coherentpath/explorer/releases/download/v0.12.0-gcs.1/libexplorer-v0.12.0-gcs.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:de1f8520ba26e2c0cf72f2a6cac35dd85e8e90dd69ef50b6eb1822f28076152c",
+  "https://github.com/coherentpath/explorer/releases/download/v0.12.0-gcs.1/libexplorer-v0.12.0-gcs.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:edc592b5f3dc9d207ab38dcde3b19ec5d52011bfdae8c2a0ae5335748f8ab6ac",
+  "https://github.com/coherentpath/explorer/releases/download/v0.12.0-gcs.1/libexplorer-v0.12.0-gcs.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:950cdbde3ad2673efdcb3e483464e00a388e92303b7b64c9c56deaf01cad28a9"
+}
